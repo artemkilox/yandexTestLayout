092facf514f5b469263f8ef547435a7983b54f14
Artem Chernykh <artemchernykh001@mail.ru>
2024-01-28 05:30:14 +0300

    almost 3 pages

---
+++ app.js
@@ -0,0 +1,16 @@
+// const runLine = document.getElementById('runLine')
+// const elements = [
+//     '<div id="runItem1" class="text-line">Дело помощи утопающим — дело рук самих утопающих!</div>',
+//     '<div id="runItem2" class="dot"></div><div class="dot">•</div>',
+//     '<div id="runItem3" class="text-line">Шахматы двигают вперед не только культуру, но и экономику!</div>',
+//     '<div id="runItem4" class="dot"></div><div class="dot">•</div>',
+//     '<div id="runItem5" class="text-line">Лед тронулся, господа присяжные заседатели!</div>'
+// ]
+// let iterator = 1
+// setInterval(() => {
+//     runLine.innerHTML += elements[iterator]
+//     document.getElementById('runItem' + iterator).remove()
+//     iterator++
+//     if(iterator === 6)
+//         iterator = 1
+// }, 1000)
