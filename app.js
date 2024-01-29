b2835b86a90e107deca9d2e0994419929fe271c7
Artem Chernykh <artemchernykh001@mail.ru>
2024-01-29 03:44:05 +0300

    complete desktop, start mobile

--- app.js
+++ app.js
@@ -1,16 +1,71 @@
-// const runLine = document.getElementById('runLine')
-// const elements = [
-//     '<div id="runItem1" class="text-line">Дело помощи утопающим — дело рук самих утопающих!</div>',
-//     '<div id="runItem2" class="dot"></div><div class="dot">•</div>',
-//     '<div id="runItem3" class="text-line">Шахматы двигают вперед не только культуру, но и экономику!</div>',
-//     '<div id="runItem4" class="dot"></div><div class="dot">•</div>',
-//     '<div id="runItem5" class="text-line">Лед тронулся, господа присяжные заседатели!</div>'
-// ]
-// let iterator = 1
-// setInterval(() => {
-//     runLine.innerHTML += elements[iterator]
-//     document.getElementById('runItem' + iterator).remove()
-//     iterator++
-//     if(iterator === 6)
-//         iterator = 1
-// }, 1000)
+///////// PLAYERS CAROUSEL ////////
+const backButton    = document.getElementById('backButton')
+const curPage       = document.getElementById('curPage')
+const nextButton    = document.getElementById('nextButton')
+const carousel      = document.getElementById('carousel')
+const positions = [
+    45,
+    -365,
+    -775,
+    -1185,
+]
+let currentPosition = 0
+let forward = 1
+const backPage = () => {
+    currentPosition--
+    let number = Number(curPage.innerHTML)
+    number--
+    curPage.innerHTML = `${number}`
+    if (currentPosition === 0){
+        backButton.classList.add('disabled')
+    } else if (currentPosition !== 0){
+        backButton.classList.remove('disabled')
+    }
+    if (currentPosition === 3){
+        nextButton.classList.add('disabled')
+    } else if (currentPosition !== 3){
+        nextButton.classList.remove('disabled')
+    }
+    carousel.style.marginLeft = `${positions[currentPosition]}px`
+}
+const nextPage = () => {
+    currentPosition++
+    let number = Number(curPage.innerHTML)
+    number++
+    curPage.innerHTML = `${number}`
+    if (currentPosition === 0){
+        backButton.classList.add('disabled')
+    } else if (currentPosition !== 0){
+        backButton.classList.remove('disabled')
+    }
+    if (currentPosition === 3){
+        nextButton.classList.add('disabled')
+    } else if (currentPosition !== 3){
+        nextButton.classList.remove('disabled')
+    }
+    carousel.style.marginLeft = `${positions[currentPosition]}px`
+}
+backButton.addEventListener('click', () => {
+    if(currentPosition > 0){
+        backPage()
+    }
+})
+nextButton.addEventListener('click', () => {
+    if(currentPosition < 3){
+        nextPage()
+    }
+})
+setInterval(() => {
+    if(forward === 1 && currentPosition < 3){
+        nextPage()
+    } else if(forward === 0 && currentPosition > 0){
+        backPage()
+    } else if(currentPosition === 0){
+        forward = 1
+        nextPage()
+    } else if(currentPosition === 3){
+        forward = 0
+        backPage()
+    }
+}, 4000)
+//////////////////////////////////////////////////////////
